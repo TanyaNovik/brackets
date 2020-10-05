574073c331b947d3d7d6399b2316f5b8a7737d09
Tatsiana Novik <Tatsiana_Novik@epam.com>
2020-10-05 19:41:01 +0300

    init: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for(let i=0;i<bracketsConfig.length;) {
+        let regExp ;
+        if(bracketsConfig[i][0].match(/\d/)) {
+            regExp = new RegExp(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`, 'g');
+        } else {
+            regExp = new RegExp('\\' + bracketsConfig[i][0] + '\\' + bracketsConfig[i][1], 'g');
+        }
+        if(str.match(regExp)){
+            str = str.replace(regExp, '');
+            i=0;
+        } else {
+            i++;
+        }
+    }
+    return !str;
 }
